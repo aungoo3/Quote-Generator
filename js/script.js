d54cca3ea545d94e49f0df2b52bf6ddda52a429b
Aung <104800264+aungoo3@users.noreply.github.com>
2023-09-17 08:16:13 -0400

    second commit

--- js/script.js
+++ js/script.js
@@ -11,34 +11,48 @@ project 1 - A Random Quote Generator
  * `quotes` array 
 ***/
 const quotes = [
-{
-  quote: "To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.",
-  author: "Ralph Waldo Emerson"
-},
-{
-  quote: "The only way to do great work is to love what you do.",
-  author: "Steve Jobs"
-},
-{
-  quote: "In three words I can sum up everything I've learned about life: it goes on.",
-  author:  "Robert Frost"
-},
-{
-  quote: "The only thing necessary for the triumph of evil is for good men to do nothing.",
-  author: " Edmund Burke "
-},
-{
-  quote: "The best way to predict the future is to create it.",
-  author:  " Peter Drucker "
-},
-{
-  quote: "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.",
-  author: " Albert Einstein "
-},
-{
-  quote: "In the end, we will remember not the words of our enemies, but the silence of our friends.",
-  author:  "Martin Luther King Jr. "
-}
+  {
+    quote: "To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.",
+    author: "Ralph Waldo Emerson",
+    citation: "Self-Reliance",
+    year: "1941"
+  },
+  {
+    quote: "The only way to do great work is to love what you do.",
+    author: "Steve Jobs",
+    citation: "Stanford University Commencement Address",
+    year: "2005"
+  },
+  {
+    quote: "In three words I can sum up everything I've learned about life: it goes on.",
+    author: "Robert Frost",
+    citation: "The Pasture",
+    year: "1915"
+  },
+  {
+    quote: "The only thing necessary for the triumph of evil is for good men to do nothing.",
+    author: "Edmund Burke",
+    citation: "Thoughts on the Cause of the Present Discontents",
+    year: "1770"
+  },
+  {
+    quote: "Success is not final, failure is not fatal: It is the courage to continue that counts.",
+    author: "Winston Churchill",
+    citation: "Speech at Harrow School",
+    year: "1941"
+  },
+  {
+    quote: "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+    author: "Nelson Mandela",
+    citation: "Long Walk to Freedom",
+    year: "1994"
+  },
+  {
+    quote: "Darkness cannot drive out darkness; only light can do that. Hate cannot drive out hate; only love can do that.",
+    author: "Martin Luther King Jr.",
+    citation: "Strength to Love",
+    year: "1963"
+  }
 ];
 
 
@@ -60,10 +74,10 @@ function printQuote() {
   let html = 
   `
     <p class = "quote"> ${ randomQuote['quote'] } </p>
-    <p class = "source"> ${ randomQuote['author'] } </p>
+    <p class = "source"> ${ randomQuote['author'] }<span class = "citation"> ${ randomQuote['citation'] }</span><span class = "year"> ${ randomQuote['year'] } </span> </p>
   `;
   
-  document.getElementById('quote-box').innerHTML = html
+  document.getElementById('quote-box').innerHTML = html ;
 
   return randomQuote;
 }
