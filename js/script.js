ed963e119890d887483a3bfa94ed90662367c060
Aung <104800264+aungoo3@users.noreply.github.com>
2023-09-18 20:45:47 -0400

    Updating all the necessaries

--- js/script.js
+++ js/script.js
@@ -15,13 +15,15 @@ const quotes = [
     quote: "To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.",
     author: "Ralph Waldo Emerson",
     citation: "Self-Reliance",
-    year: "1941"
+    year: "1941",
+    tags: "Inspiration"
   },
   {
     quote: "The only way to do great work is to love what you do.",
     author: "Steve Jobs",
     citation: "Stanford University Commencement Address",
-    year: "2005"
+    year: "2005",
+    tags: "Motivation"
   },
   {
     quote: "In three words I can sum up everything I've learned about life: it goes on.",
@@ -39,7 +41,8 @@ const quotes = [
     quote: "Success is not final, failure is not fatal: It is the courage to continue that counts.",
     author: "Winston Churchill",
     citation: "Speech at Harrow School",
-    year: "1941"
+    year: "1941",
+    tags: "Motivation"
   },
   {
     quote: "The greatest glory in living lies not in never falling, but in rising every time we fall.",
@@ -51,7 +54,8 @@ const quotes = [
     quote: "Darkness cannot drive out darkness; only light can do that. Hate cannot drive out hate; only love can do that.",
     author: "Martin Luther King Jr.",
     citation: "Strength to Love",
-    year: "1963"
+    year: "1963",
+    tags: "Inspiration"
   }
 ];
 
@@ -84,20 +88,44 @@ function printQuote() {
   const randomQuote = getRandomQuote();
   const randomColor = randomRGB();
   
-  let html = 
-  `
-    <p class = "quote"> ${ randomQuote['quote'] } </p>
-    <p class = "source"> ${ randomQuote['author'] }<span class = "citation"> ${ randomQuote['citation'] }</span><span class = "year"> ${ randomQuote['year'] }</span> 
-    </p>
-  `;
-  
-  document.getElementById('quote-box').innerHTML = html ;
-  
+  let html = '';
+
+  // This check if the 'quote' property exists
+  if (randomQuote['quote']) {
+    html += `<p class="quote">${randomQuote['quote']}</p>`;
+  }
+
+  // This check if the 'author' property exists
+  if (randomQuote['author']) {
+    html += `<p class="source">${randomQuote['author']}`;
+  }
+
+  // This check if the 'citation' property exists
+  if (randomQuote['citation']) {
+    html += `<span class="citation">${randomQuote['citation']}</span>`;
+  }
+
+  // This check if the 'year' property exists
+  if (randomQuote['year']) {
+    html += `<span class="year">${randomQuote['year']}</span>`;
+  }
+
+  // This check if the 'tags' property exists
+  if (randomQuote['tags']) {
+    html += `<div class="tags">Tags: ${randomQuote['tags']}</div>`;
+  }
+
+  // This close the 'source' paragraph
+  if (randomQuote['author']) {
+    html += `</p>`;
+  }
+
+  document.getElementById('quote-box').innerHTML = html;
   document.body.style.backgroundColor = randomColor;
 
   return randomQuote;
-
 }
+
 
 
 /**
@@ -105,11 +133,10 @@ function printQuote() {
  * I give credit to https://www.geeksforgeeks.org/how-to-automatic-refresh-a-web-page-in-fixed-time/
  ***/
 
-
 function autoRefresh() {
   window.location = window.location.href;
 }
-setInterval(printQuote, 7000);
+setInterval(printQuote, 7000); 
 
 
 /***
